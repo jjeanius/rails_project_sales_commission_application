1bd4e78d267a65292f50955094d34339fa77d839
jjeanius <jjeanius@verizon.net>
2019-05-08 00:12:33 -0400

    adding resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,16 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
   devise_for :employees
+
+  resources :employees
+  resources :products
+  resources :sales
+
+
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
