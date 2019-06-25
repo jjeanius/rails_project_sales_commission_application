88e917c5a5361accc63653d6b2b26ecfa2feddd1
Judy Jean McGoldrick <jjeanius@verizon.net>
2019-06-25 01:36:07 -0400

    Update callbacks_controller.rb

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,13 +2,8 @@ class CallbacksController < Devise::OmniauthCallbacksController   #inherit from 
    
   def amazon  #find or create an employee matching that hash 
     @employee = Employee.from_omniauth(request.env["omniauth.auth"])
-    if @employee.persisted?
-      sign_in_and_redirect @employee
+       sign_in_and_redirect @employee
         flash.notice = "Signed in!"
-    else
-      session["devise.employee_attributes"] = employee.attributes
-        redirect_to new_employee_registration_url     
-    end
-    
+   
   end
 end
