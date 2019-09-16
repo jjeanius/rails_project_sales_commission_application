bb236fe028ef4a71cb44881f81450291636a43b9
judy <jjeanius@verizon.net>
2019-09-16 09:45:56 -0400

    changing gem lock for devise

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,8 +1,9 @@
 class CallbacksController < Devise::OmniauthCallbacksController
 
   def amazon
-    @employee = Employee.from_omniauth(request.env["omniauth.auth"])
+    @employee = Employee.from_omniauth(request.env["omniauth.auth"])  #  retieved info can be found in hash
       sign_in_and_redirect @employee   # sign_in_and_redirect - a Devise method
     flash.notice = "Signed in!"
   end
 end
+i
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get 'sales/highest_commission', to: 'sales#highest_commission'
-  
+
   get 'sales/quantity', to: 'sales#quantity'
 
 
